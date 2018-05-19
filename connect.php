d7830dfdec18564edd31e7e04db16f45fa331e6c
Andrea Lara <anlara@csumb.edu>
2018-05-19 07:03:19 +0000

    proper connection to heroku

--- connect.php
+++ connect.php
@@ -8,6 +8,19 @@ if(!$con){
 if(!mysqli_select_db($con,'final_project')){
     echo 'Database Not selected';
 }
+    $host = "us-cdbr-iron-east-04.cleardb.net";
+    $dbName = "heroku_981fc0aa0d1b350";
+    $username = "bdc992bb9af150";
+    $password = "29b5a64a";
+//when connecting from Heroku
+    //mysql://bdc992bb9af150:29b5a64a@us-cdbr-iron-east-04.cleardb.net/heroku_981fc0aa0d1b350?reconnect=true
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbName = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    }
 $Email = $_POST['email'];
 $Name = $_POST['uName'];
 $Pswrd = $_POST['pass'];
